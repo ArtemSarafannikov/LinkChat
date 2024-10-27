1d1b7a5660d1e96ad63511056eeec8a362111ea9
ArtemSarafannikov <sarafannikov.artem@yandex.ru>
2024-10-27 20:24:03 +0300

    feat: save to db

--- linkchat/chat/models.py
+++ linkchat/chat/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Message(models.Model):
+    username = models.CharField(max_length=255)
+    text = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.username}: {self.text} at {self.timestamp}"
